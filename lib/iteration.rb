f4fddab29798954baf6ca2ddf1e60419aa3ec398
Alessandro Duterte <alexduterte@gmail.com>
2020-01-29 00:27:48 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -10,7 +10,7 @@ def join_ingredients(src)
   joined = []
   while i < src.count do
     element = src[i]
-    joined << 'I love #{element[0]} and #{element[1]} on my pizza'
+    joined << 'I love #{src[i][0]} and #{src[i][1]} on my pizza'
     i += 1
   end
   joined
